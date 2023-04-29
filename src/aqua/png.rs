19e7b1c558da44d93a603aa3b7e70482f2188537
obiwac <obiwac@gmail.com>
2023-04-29 22:56:37 +0200

    png: Fix unused `mut`

--- src/aqua/png.rs
+++ src/aqua/png.rs
@@ -17,7 +17,7 @@ impl Png {
 	pub fn from_path(path: &str) -> Png {
 		let dev = aqua::query_device("aquabsd.alps.png");
 
-		let mut buf = std::fs::read(path).expect("can't open file");
+		let buf = std::fs::read(path).expect("can't open file");
 
 		let png = aqua::send_device!(dev, 0x6C64, buf.as_ptr());
 
