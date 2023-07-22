93a5fc412ff058a61798b73388267dbc67561f23
obiwac <obiwac@gmail.com>
2023-07-22 11:29:44 +0200

    win: Add documentation comments

--- src/win.rs
+++ src/win.rs
@@ -16,6 +16,7 @@ enum Cb {
 
 type WinDrawHook = unsafe extern "C" fn(win: u64, data: u64) -> u64;
 
+/// An `aquabsd.alps.win` window object.
 pub struct Win {
 	dev: ::Device,
 	pub win: u64,
@@ -37,6 +38,18 @@ fn get_draw_trampoline<F>(_closure: &F) -> WinDrawHook where
 }
 
 impl Win {
+	/// Create a new window with the given resolution.
+	///
+	/// # Arguments
+	///
+	/// * `x_res` - Horizontal resolution of the window, in pixels.
+	/// * `y_res` - Vertical resolution of the window, in pixels.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// let win = aqua::win::Win::new(800, 600);
+	/// ```
 	pub fn new(x_res: u32, y_res: u32) -> Self {
 		let dev = ::query_device("aquabsd.alps.win");
 		let win = ::send_device!(dev, Cmd::Create, x_res, y_res);
@@ -44,11 +57,37 @@ impl Win {
 		Win { dev, win }
 	}
 
+	/// Set the caption of the window.
+	///
+	/// # Arguments
+	///
+	/// * `name` - Caption to be given.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// let mut win = aqua::win::Win::new(800, 600);
+	/// win.caption("This is a Caption");
+	/// ```
 	pub fn caption(&mut self, name: &str) {
 		let c_str = std::ffi::CString::new(name).unwrap();
 		::send_device!(self.dev, Cmd::SetCaption, self.win, c_str.as_ptr());
 	}
 
+	/// Starts the draw loop of the window.
+	///
+	/// # Arguments
+	///
+	/// * `closure` - Closure to be called each time the window is drawn.
+	///
+	/// # Examples
+	///
+	/// ```
+	/// let mut win = aqua::win::Win::new(800, 600);
+	/// win.draw_loop(|| {
+	///    0
+	/// });
+	/// ```
 	pub fn draw_loop<F>(&mut self, closure: F) where
 		F: FnMut() -> u64,
 	{
